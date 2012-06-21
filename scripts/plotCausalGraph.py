0b74d61b415b17ec0df63df810def9228429e41e
Xiaopeng Zhu <nimezhu@gmail.com>
2012-06-21 14:20:23 -0500

    update plotCausalGraph with nice layout and colormap for line

--- scripts/plotCausalGraph.py
+++ scripts/plotCausalGraph.py
@@ -12,27 +12,52 @@ def show_help():
 def Main():
     if len(sys.argv)==1: show_help()
 
-    opts,restlist = getopt(sys.argv[1:],"m:oh",\
-                        ["matrix=","other","help"])
+    opts,restlist = getopt(sys.argv[1:],"m:oht:",\
+                        ["matrix=","threshold=","help"])
+    threshold=0.5
     for o, a in opts:
         if o in ("-m","matrix"): M = a
         if o in ("-h","--help"): show_help()
+        if o in ("-t","--threshold"): threshold=float(a)
     try:
         f=open(M)
     except:
         print >>sys.stderr,"Can't open file",M
         show_help()
+    max=0
     nodes=[]
     edges=[]
+    pos={}
     edges_col=[]
+    col={}
+    
+    rank={}
     lines=f.readlines()
+    i=0
     for line in lines:
         line=line.strip()
         if line[0]=="#":continue
         a=line.split("\t")
         nodes.append(a[0])
+        rank[a[0]]=0
+        for k,x in enumerate(a[1:]):
+            if k==i: continue
+            x=float(x)
+            if x>threshold or x<-threshold:
+                if k < len(nodes) and rank[a[0]] < rank[nodes[k]]+1:
+                    rank[a[0]]=rank[nodes[k]]+1
+        if col.has_key(rank[a[0]]):
+            col[rank[a[0]]]+=1
+        else:
+            col[rank[a[0]]]=1
+        pos[a[0]]=(rank[a[0]],col[rank[a[0]]])
+        i+=1
+
+
     j=0
  
+    G=nx.DiGraph()
+    G.add_nodes_from(nodes)
     for line in lines:
         line=line.strip()
         if line[0]=="#":continue
@@ -40,20 +65,19 @@ def Main():
         for i,x in enumerate(a[1:]):
             if i==j:continue
             x=float(x)
-            if x>0.5:
-                edges.append((nodes[i],nodes[j]))
-                edges_col.append("g")
-            elif x<0.5:
-                edges.append((nodes[i],nodes[j]))
-                edges_col.append("r")
+            if max<abs(x):max=abs(x)
+            if x>threshold:
+                G.add_edge(nodes[i],nodes[j],weight=x)
+                edges_col.append(x)
+            elif x<-threshold:
+                G.add_edge(nodes[i],nodes[j],weight=x)
+                edges_col.append(x)
         j+=1
 
-
-    G=nx.DiGraph()
-    G.add_nodes_from(nodes)
-    G.add_edges_from(edges)
-    nx.draw(G,edge_color=edges_col)
+    nx.draw(G,edge_cmap=plt.get_cmap("RdYlGn"),edge_color=edges_col,pos=pos,node_color="y",edge_vmin=-max,edge_vmax=max,linewidths=0)
+    plt.colorbar()
     plt.show()
+
 
 
 
