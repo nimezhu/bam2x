e0854dfee38f2edbc38cd5942b90544a16b33931
Xiaopeng Zhu <nimezhu@gmail.com>
2012-07-02 17:15:42 -0500

    add bam2transcript.py in sandbox

--- sandbox/bam2transcript.py
+++ sandbox/bam2transcript.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # Programmer : zhuxp
 # Date: 
-# Last-modified: 02 Jul 2012 15:22:41
+# Last-modified: 02 Jul 2012 17:08:04
 import os,sys,argparse
 import pysam
 import prob
 from ghmm import *
+'''
 
+'''
 class BamBins:
     '''
         count the reads number , not the coverage.
@@ -28,6 +30,9 @@ class BamBins:
             n_bin=l/self.binsize
             if l%self.binsize!=0: n_bin+=1
             self.bins.append([0 for row in range(n_bin)])
+        self.chr2tid={}
+        for i,chr in enumerate(self.chrs):
+            self.chr2tid[chr]=i
         if ReadFile:
             self.mapped=0
             self.unmapped=0
@@ -58,7 +63,6 @@ class BamBins:
             bin_id=(s.pos+s.qlen/2)/self.binsize
             self.bins[s.tid][bin_id]+=1
         print >>sys.stderr,"\rreading ",self.bamfilename," done\t\t\t"
-    
     def callBinPeak(self,pvalue=1e-05):
         self.pvalue=pvalue
         self.binpeak=[]
@@ -67,13 +71,14 @@ class BamBins:
             if prob.poisson_cdf(threshold,self.lam,False) < pvalue: break
             threshold+=1
         for i in range(len(self.bins)):
-            print >>sys.stderr,"Call Pvalue of ",self.chrs[i],"\r",
+            print >>sys.stderr,"Call Bins Pvalue in",self.chrs[i],"\r",
             self.binpeak.append([False for row in range(len(self.bins[i]))])
             for j in range(len(self.bins[i])):
                 if self.bins[i][j]>=threshold:
                # if prob.poisson_cdf(self.bins[i][j],self.lam,False)<pvalue:
                # if prob.poisson_cdf(self.bins[i][j],self.lam,False)<pvalue:
                     self.binpeak[i][j]=True
+        print >>sys.stderr,"Call Bins Pvalue Done                         "
     def PrintBinPeak(self,output="stdout"):
          if output=="stdout":
              out=sys.stdout
@@ -152,6 +157,44 @@ class BamBins:
                         state=0
             if state==1:
                  yield (self.chrs[i],start*self.binsize,(stop+1)*self.binsize)
+    def iterPeaks(self):
+        self.peaks=[]
+        for i in self.parse_segment():
+            (chr,start,stop)=i
+        
+            a=self.samfile.fetch(chr,start,stop)
+            refine_start=None
+            refine_stop=None
+            reads_num=0
+            for j in a:
+                reads_num+=1
+                if refine_start==None:
+                    refine_start=j.pos
+                if refine_stop==None:
+                    refine_stop=j.pos+j.qlen
+                if j.pos < refine_start: refine_start=j.pos
+                if j.pos+j.qlen > refine_stop: refine_stop=j.pos+j.qlen
+            peak_intensity=0
+            peak_pos=0
+            coverage=0.0
+            for pileupcolumn in self.samfile.pileup(chr,refine_start,refine_stop):
+                
+                if pileupcolumn.n > peak_intensity:
+                    peak_intensity=pileupcolumn.n
+                    peak_pos=pileupcolumn.pos
+                coverage+=pileupcolumn.n
+            coverage/=(refine_stop-refine_start)
+            lam=float(self.mapped)/(self.total_length/(refine_stop-refine_start))
+
+            pvalue=prob.poisson_cdf(reads_num,lam,False)
+            yield (chr,refine_start,refine_stop,reads_num,pvalue,coverage,peak_pos,peak_intensity)
+
+
+
+
+
+
+
 
 
 
@@ -161,24 +204,90 @@ class BamBins:
 def ParseArg():
     ''' This Function Parse the Argument '''
     p=argparse.ArgumentParser( description = 'Example: %(prog)s -i file.bam -o file.bed', epilog='Library dependency : pysam')
-    p.add_argument('-v','--version',action='version',version='%(prog)s 0.1')
-    p.add_argument('--input','-i',type=str,dest='Bamfile',help="the input alignment bamfile")
+    p.add_argument('-v','--version',action='version',version='%(prog)s 0.1')#
+#   p.add_argument('--input','-i',type=str,dest='Bamfile',help="the input alignment bamfile")
+    p.add_argument('--elongation','-E',type=str,dest='eBamfile',help="the input alignment bamfile of H3K36me3")
+    p.add_argument('--promoter','-P',type=str,dest='pBamfile',help="the input alignment bamfile of H3K4me3 (or H3K27ac etc.)")
     p.add_argument('--output','-o',type=str,dest='Output',default="stdout", help="the output bed file  default: %(default)s")
     p.add_argument('--pvalue','-p',type=float,dest='Pvalue',default=1e-05, help="p-value threshold  default: %(default)f")
-    p.add_argument('--bin','-b',type=int,dest='Binsize',default=200, help="binsize  default: %(default)f")
+    p.add_argument('--bin','-b',type=int,dest='Binsize',default=200, help="binsize default: %(default)d")
+    p.add_argument('--near','-n',type=int,dest='near',default=5000, help="Promoter less than  %(default)d are counted as Nearby Promoter")
     if len(sys.argv)==1:
         print >>sys.stderr,p.print_help()
         exit(0)
 
     return p.parse_args()
+
+
+binOffsets=(512+64+8+1,64+8+1,8+1,1,0)
+binFirstShift=17
+binNextShift=3
+
+
+def binFromRangeStandard(start,end):
+    startBin=start
+    endBin=end-1 
+    startBin >>= binFirstShift
+    endBin >>= binFirstShift
+    for i in range(len(binOffsets)):
+        if startBin==endBin:
+            return binOffsets[i]+startBin
+        startBin >>= binNextShift
+        endBin >>= binNextShift
+    print >>sys.stderr,"start %d , end %d out of range"%(start,end) 
+    return 0
+def rangeIterOverlapBins(start,end):
+    startBin=start
+    endBin=end-1
+    startBin >>= binFirstShift
+    endBin >>= binFirstShift
+    for i in range(len(binOffsets)):
+        for j in range(startBin,endBin+1):
+            yield j+binOffsets[i]
+        startBin >>= binNextShift
+        endBin >>= binNextShift
+    
+    
+    
+def iterNearPromoter(chrom,start,stop):
+    near_start=start-args.near
+    near_stop=stop+args.near
+    if near_start<0:
+        near_start=0
+    for bin in rangeIterOverlapBins(near_start,near_stop):
+        for p in pTable[chrom][bin]:
+                if (p[0] < stop and p[1] > start):
+                    yield p
+
+def tab(a):
+    s=str(a[0])
+    for i in a[1:]:
+        s+="\t"+str(i)
+    return s
 def Main():
-    global args,Table
+    global args,Table,pTable
+    near=5000
     args=ParseArg()
-    bambin=BamBins(args.Bamfile,args.Binsize)
-    bambin.process(args.Pvalue)
-    print bambin.m
-    for i in bambin.parse_segment():
-        print i
+    ebambin=BamBins(args.eBamfile,args.Binsize)
+    ebambin.process(args.Pvalue)
+    pbambin=BamBins(args.pBamfile,args.Binsize)
+    pbambin.process(args.Pvalue)
+    pTable={}
+    eTable={}
+    for i in ebambin.chrs:
+        eTable[i]=[[] for j in range(4096+512+64+8+1)]
+        pTable[i]=[[] for j in range(4096+512+64+8+1)]
+    for i in pbambin.iterPeaks():
+        (chrom,start,stop,reads_num,pvalue,coverage,peak_pos,peak_i)=i
+        b=binFromRangeStandard(start,stop)
+        pTable[chrom][b].append(i)
+    for i in ebambin.iterPeaks():
+        (chrom,start,stop,reads_num,pvalue,coverage,peak_pos,peak_i)=i
+        print tab(i) 
+        for p in iterNearPromoter(chrom,start,stop):
+                print "\t",tab(p)
+
+
   
         
    
