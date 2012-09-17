fbd28d366e267ab28028f875d3480f3e4c12a6c7
Xiaopeng Zhu <nimezhu@gmail.com>
2012-09-16 22:28:40 -0500

    xbam2bed.py new version

--- bin/xbam2bed.py
+++ bin/xbam2bed.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python
 # Programmer : zhuxp
 # Date: 
-# Last-modified: 07-17-2012, 13:28:40 CDT
+# Last-modified: 16 Sep 2012 22:27:38
 
 import os,sys,argparse
-import pysam
+from xplib.Annotation import Bed
+from xplib import TableIO
 def ParseArg():
     ''' This Function Parse the Argument '''
     p=argparse.ArgumentParser( description = 'Example: %(prog)s -h', epilog='Library dependency : pysam')
     p.add_argument('-v','--version',action='version',version='%(prog)s 0.1')
-    p.add_argument('-i','--input',dest="input",type=str,help="bamfile")
-    p.add_argument('-o','--output',dest="output",type=str,default="stdout",help="output bed file")
+    p.add_argument('-i','--input',dest="input",type=str,help="input file")
+    p.add_argument('-o','--output',dest="output",type=str,default="stdout",help="output file")
     
     if len(sys.argv)==1:
         print >>sys.stderr,p.print_help()
@@ -27,18 +28,12 @@ def Main():
         except IOError:
             print >>sys.stderr,"can't open file ",args.output,"to write. Using stdout instead"
             out=sys.stdout
-    bam=pysam.Samfile(args.input,"rb")
-    for x,i in enumerate(bam):
-        if x%100000==0: print >>sys.stderr,"converted",x,"reads\r",
-        if i.tid<0: continue
-        print >>out,bam.references[i.tid]+"\t"+str(i.pos)+"\t"+str(i.pos+i.alen)
-
-
-
-
-    
+    s=TableIO.parse(args.input,"bam2bed")
+    for i in s:
+        print >>out,i
 if __name__=="__main__":
     Main()
+
 
 
 
