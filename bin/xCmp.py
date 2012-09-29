e125365efb80c7a1c833dc270598cdc3a115c41a
Xiaopeng Zhu <nimezhu@gmail.com>
2012-09-28 22:28:33 -0500

    DBI xCmp.py Version

--- bin/xCmp.py
+++ bin/xCmp.py
@@ -1,31 +1,30 @@
 #!/usr/bin/python
 # Programmer : zhuxp
 # Date: 
-# Last-modified: 20 Sep 2012 12:56:52
+# Last-modified: 28 Sep 2012 22:26:23
 
 import os,sys,argparse
 from xplib.Annotation import *
 from xplib import TableIO
+from xplib import DBI
+
+
 def ParseArg():
     ''' This Function Parse the Argument '''
     p=argparse.ArgumentParser( description = 'Example: %(prog)s -i [query.bed|query.genebed|query.OddsRatio.out] --input-format [bed|genebed|oddsratiosnp] -a file1.genebed file2.genebed --db_format genebed', epilog='Library dependency : xplib')
-    p.add_argument('-v','--version',action='version',version='%(prog)s 0.2')
-    p.add_argument('-i','--input',dest="input",type=str,help="input file")
+    p.add_argument('-v','--version',action='version',version='%(prog)s 0.3')
+    p.add_argument('-i','--input',dest="input",type=str,default="stdin",help="input file")
     p.add_argument('-I','--input_format',dest="input_format",action="store",default="bed",help="input file format")
     p.add_argument('-o','--output',dest="output",type=str,default="stdout",help="output file")
     p.add_argument('-a','--annotations',dest="db",action="store",default=[],help="feature annotation files",nargs="+")
-    p.add_argument('-A','--annotations_format',dest="db_format",action="store",default=[],help="annotation files format",nargs="+")
+    p.add_argument('-A','--annotations_format',dest="db_format",action="store",default=[],help="annotation files formati [bed|genebed|tabix|vcf]",nargs="+")
     p.add_argument('-m',dest="m",action="store_true",default=False,help="print in table format")
-
-    
     if len(sys.argv)==1:
         print >>sys.stderr,p.print_help()
         exit(0)
     return p.parse_args()
 def Main():
     global args,out
-    h={} #count the hits number
-    hcode={} #count the hits number
     args=ParseArg()
     if args.output=="stdout":
         out=sys.stdout
@@ -35,6 +34,11 @@ def Main():
         except IOError:
             print >>sys.stderr,"can't open file ",args.output,"to write. Using stdout instead"
             out=sys.stdout
+    if args.input=="stdin":
+        input=sys.stdin
+    else:
+        input=open(args.input,"r")
+
     db_format=args.db_format
     if len(db_format)==0:
         db_format=["bed" for i in range(len(args.db))]
@@ -45,71 +49,48 @@ def Main():
         print >>sys.stderr,"db format",db_format
         print >>sys.stderr,"db ",args.db
         exit(0)
-    datas=[]
-    input_handle=TableIO.parse(args.input,args.input_format)
-    beds=[]
-    h[args.input]=0
-    for i in input_handle:
-        beds.append(i)
-        h[args.input]+=1
        
-    for i,f in enumerate(args.db):
-        print >>sys.stderr,"reading",f
-        h[f]=0
-        fi=TableIO.parse(f,db_format[i])
-        '''
-        data tuple structure
-        (filename,data_binindex)
-        usage example:
-            bed=Bed([chr,start,end])
-            for (file,data) in data:
-                for feature in Utils.iterOverlapFeature(bed,data):
-                    print feature
-        '''
-        datas.append((f,Utils.readIntoBinIndex(fi)))
     print >>out,"# Input:",args.input
+    dbis=[]
+    hits=[]  #count the hits
+    hcode={}
     for i,f in enumerate(args.db):
-        print >>out,"# Feature No."+str(i+1),":",f
-
-    for i in beds:
-
-        print >>out,i,
-        code=""
+        print >>out,"# Features File No."+str(i+1),":",f
+        dbis.append(DBI.init(f,db_format[i]))
+        hits.append(0)
+    query_num=0
+    for bed in TableIO.parse(input,args.input_format):
         if not args.m:
-            print >>out,""
-            print >>out,""
-        for (f,d) in datas:
-            s=""
-            mark=0
-            for feature in Utils.iterOverlapFeature(i,d):
-                mark+=1
-                s+=str(feature)+"\n"
-            if mark>0:
-                h[f]+=1
-                code+="1"
+            print >>out,"QR\t",bed
+        query_num+=1
+        code="@"
+        for i,dbi in enumerate(dbis):
+            flag=0
+            for hit in DBI.query(bed,dbi):
                 if not args.m:
-                    print >>out,"OVERLAP FEATURES IN",f
-                    print >>out,s
-                else:
-                    print >>out,"\t",1,
-            else:
-                code+="0"
-                if not args.m:
-                    print >>out,"NO OVERLAP FEATURES IN",f
-                else:
-                    print >>out,"\t",0,
+                    print >>out,"\tDB"+str(i+1)+" HT\t",hit
+                flag=1
+            hits[i]+=flag
+            code+=str(flag)
         if hcode.has_key(code):
             hcode[code]+=1
         else:
             hcode[code]=1
-        if args.m:
-            print >>out,"\t","#"+code
+        if not args.m:
+            print >>out,"CD "+code,"\t",bed
+            print >>out,""
+            print >>out,""
         else:
-            print >>out,"//"
-    for i,f in enumerate(args.db):
-        print >>out,"# ",h[f],"/",h[args.input],"overlap with No."+str(i+1),f
-    for code in sorted(hcode.iterkeys()):
-        print >>out,"# code:",code,"\t",hcode[code]
+            print >>out,bed,"\t","CD "+code
+
+    for i,x in enumerate(hits):
+        print >>out,"#",x,"/",query_num,"overlap with No."+str(i+1),args.db[i]
+
+    for key in sorted(hcode.keys()):
+        print >>out,"# code:"+key,"\t",hcode[key]
+
+
+
 
     
 if __name__=="__main__":
