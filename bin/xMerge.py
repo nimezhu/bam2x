70fd5bcd6ee464db4b97e59a1dfff70f09bb1746
Xiaopeng Zhu <nimezhu@gmail.com>
2012-12-03 20:37:35 -0600

    update xMerge.py to binindex structure

--- bin/xMerge.py
+++ bin/xMerge.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python
 # Programmer : zhuxp
 # Date: 
-# Last-modified: 20 Sep 2012 13:06:47
+# Last-modified: 03 Dec 2012 20:35:35
 
 import os,sys,argparse
 from xplib.Annotation import *
 from xplib import TableIO
+from xplib.Struct import binindex
+from xplib import DBI
 def ParseArg():
     ''' This Function Parse the Argument '''
     p=argparse.ArgumentParser( description = 'Merge Annotations that have same (chromosome,start,stop,strand) and sort them to print', epilog='Library dependency : xplib')
@@ -34,28 +36,23 @@ def Main():
     
     if len(db_format)==1:
         db_format=[db_format[0] for i in range(len(args.db))]
-    data={}
+    data=binindex()
     for i,f in enumerate(args.db):
         for item in TableIO.parse(f,db_format[i]):
             flag=0
-            for feat in Utils.getOverlapFeatures(item,data):
+            for feat in data.query(item):
                 if feat==item: #define in Class.__cmp__
                     flag=1
             if not flag:
-                Utils.appendIntoBinIndex(data,item)
+                data.append(item)
     data_list=[]
-    for i in Utils.iterBinIndex(data):
+    for i in data:
         data_list.append(i)
     data_list.sort()
     for i in data_list:
         print >>out,i
 
-
-
     
 if __name__=="__main__":
     Main()
-
-
-
 
