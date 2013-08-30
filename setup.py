00053ab0e6df0729045b1bdcf5f999a7b6ed1962
zhuxp <nimezhu@gmail.com>
2013-08-30 16:43:08 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# Programmer : zhuxp
+# Date: 
+# Last-modified: 08-30-2013, 16:42:25 EDT
+import os
+import sys
+from distutils.core import setup
+def main():
+    if not float(sys.version[:3])>2.4:
+        sys.stderr.write("CRITICAL: Python version must be greater than or equal to 2.4!")
+        sys.exit(1)
+    setup(
+          name="xplib",
+          version="0.1",
+          description="bam2x lib",
+          author="Xiaopeng Zhu",
+          author_email="nimezhu@gmail.com",
+          packages=["xplib",
+                    "xplib.Annotation",
+                    "xplib.DBI",
+                    "xplib.Stats",
+                    "xplib.Struct",
+                    "xplib.TableIO",
+                    "xplib.Tools"
+                    ],
+          package_dir={"":"lib"},
+          scripts=["bin/xQuery.py"],
+          requires=["pysam","twobitreader","bx"],
+          )
+
+
+
+
+
+    
+if __name__=="__main__":
+    main()
+
+
+
+
+
