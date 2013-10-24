9b842f9ae8e7807f661b729f46901c076f9fd515
zhuxp <nimezhu@gmail.com>
2013-10-24 18:07:02 -0400

    revise xRead , now it will automatic recognize file format

--- bin/xRead.py
+++ bin/xRead.py
@@ -1,20 +1,16 @@
 #!/usr/bin/env python
 # Programmer : zhuxp
 # Date: 
-# Last-modified: 09-04-2013, 14:56:38 EDT
-'''
-This program convert bam format into bed format.
-it is a test program for TableIO.parse(file.bam,"bam2bed")
-'''
+# Last-modified: 10-24-2013, 18:06:00 EDT
 import os,sys,argparse
-from xplib.Annotation import Bed
-from xplib import TableIO
+from xplib import TableIO,Tools
+from xplib.Tools import IO
 def ParseArg():
     ''' This Function Parse the Argument '''
     p=argparse.ArgumentParser( description = 'Example: %(prog)s -h', epilog='Library dependency : pysam')
     p.add_argument('-v','--version',action='version',version='%(prog)s 0.1')
     p.add_argument('-i','--input',dest="input",type=str,help="input file")
-    p.add_argument('-I','--converter',dest="format",type=str,choices=TableIO.FormatToIterator.keys(),help="input format, converter: bed , bam2bed, bam2bed12, vcf ....")
+    p.add_argument('-I','--converter',dest="format",type=str, default="guess", choices=TableIO.FormatToIterator.keys(),help="input format, converter: bed , bam2bed, bam2bed12, vcf ....")
     p.add_argument('-o','--output',dest="output",type=str,default="stdout",help="output file")
     
     if len(sys.argv)==1:
@@ -28,14 +24,10 @@ def Main():
     '''
     global args,out
     args=ParseArg()
-    if args.output=="stdout":
-        out=sys.stdout
-    else:
-        try:
-            out=open(args.output,"w")
-        except IOError:
-            print >>sys.stderr,"can't open file ",args.output,"to write. Using stdout instead"
-            out=sys.stdout
+    fin=IO.fopen(args.input,"r")
+    out=IO.fopen(args.output,"w")
+    if args.format=="guess":
+        args.format=Tools.guess_format(args.input)
     s=TableIO.parse(args.input,args.format)
     for i in s:
         print >>out,i
